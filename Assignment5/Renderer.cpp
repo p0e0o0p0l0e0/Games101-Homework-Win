3465cd7d0ddbba76277f3cc5b4bbb91ccc27e1e6
DESKTOP-962T038\ZhangLinqi <p0e0o0p0l0e0@sina.com>
2021-08-16 00:04:34 +0800

    作业5 完成

--- Assignment5/Renderer.cpp
+++ Assignment5/Renderer.cpp
@@ -223,14 +223,27 @@ void Renderer::Render(const Scene& scene)
         for (int i = 0; i < scene.width; ++i)
         {
             // generate primary ray direction
-            float x = 0;
-            float y = 0;
+            float x;
+            float y;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
 
+
+            // 先将屏幕空间的坐标转换到[-1, 1]的标准化范围，再通过scale和imageAspectRatio恢复到在z=-1处的成像尺寸，就是世界坐标。
+            // 再和eye_pos相减计算方向。
+            x = (i + 0.5f - scene.width / 2) / (scene.width / 2);
+            y = (scene.height / 2 - j - 0.5) / (scene.height / 2);
+
+            x *= scale * imageAspectRatio;
+            y *= scale;
+
+            x -= eye_pos.x;
+            y -= eye_pos.y;
+
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
--- Assignment5/Triangle.hpp
+++ Assignment5/Triangle.hpp
@@ -4,6 +4,7 @@
 
 #include <cstring>
 
+float EPSILON = 0.00001f;
 bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f& v2, const Vector3f& orig,
                           const Vector3f& dir, float& tnear, float& u, float& v)
 {
@@ -11,6 +12,19 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    Vector3f s = orig - v0;
+    Vector3f e1 = v1 - v0;
+    Vector3f e2 = v2 - v0;
+    Vector3f s1 = crossProduct(dir, e2);
+    Vector3f s2 = crossProduct(s, e1);
+    float f = dotProduct(s1, e1);
+    tnear = dotProduct(s2, e2) / f;
+    u = dotProduct(s1, s) / f;
+    v = dotProduct(s2, dir) / f;
+    if (tnear + EPSILON >= 0 && u + EPSILON >= 0 && v + EPSILON >= 0 && (1 - u - v) + EPSILON >= 0)
+    {
+        return true;
+    }
     return false;
 }
 
