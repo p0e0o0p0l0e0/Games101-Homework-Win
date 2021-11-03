6b0d52193eb67f94e5863d70fe6b82369e456b2c
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2021-11-03 17:38:48 +0800

    作业8：创建含有三个质点的绳子

--- Assignment8/src/rope.cpp
+++ Assignment8/src/rope.cpp
@@ -8,54 +8,66 @@
 #include "spring.h"
 
 namespace CGL {
+	Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+	{
+		// TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+		Mass* startMass = new Mass(start, node_mass, false);
+		Mass* endMass = new Mass(end, node_mass, false);
+		Vector2D unitVector = (end - start) / (num_nodes - 1);
+		masses.push_back(startMass);
+		for (int i = 1; i < num_nodes - 1; i++)
+		{
+			masses.push_back(new Mass(start + unitVector * i, node_mass, false));
+		}
+		masses.push_back(endMass);
+		for (auto& i : pinned_nodes)
+		{
+			masses[i]->pinned = true;
+		}
+		for (int i = 0; i < masses.size() - 1; i++)
+		{
+			Spring* spring = new Spring(masses[i], masses[i + 1], k);
+			springs.push_back(spring);
+		}
+	}
 
-    Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
-    {
-        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+	void Rope::simulateEuler(float delta_t, Vector2D gravity)
+	{
+		for (auto& s : springs)
+		{
+			// TODO (Part 2): Use Hooke's law to calculate the force on a node
+		}
 
-//        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
-    }
+		for (auto& m : masses)
+		{
+			if (!m->pinned)
+			{
+				// TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
 
-    void Rope::simulateEuler(float delta_t, Vector2D gravity)
-    {
-        for (auto &s : springs)
-        {
-            // TODO (Part 2): Use Hooke's law to calculate the force on a node
-        }
+				// TODO (Part 2): Add global damping
+			}
 
-        for (auto &m : masses)
-        {
-            if (!m->pinned)
-            {
-                // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+			// Reset all forces on each mass
+			m->forces = Vector2D(0, 0);
+		}
+	}
 
-                // TODO (Part 2): Add global damping
-            }
+	void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+	{
+		for (auto& s : springs)
+		{
+			// TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+		}
 
-            // Reset all forces on each mass
-            m->forces = Vector2D(0, 0);
-        }
-    }
+		for (auto& m : masses)
+		{
+			if (!m->pinned)
+			{
+				Vector2D temp_position = m->position;
+				// TODO (Part 3.1): Set the new position of the rope mass
 
-    void Rope::simulateVerlet(float delta_t, Vector2D gravity)
-    {
-        for (auto &s : springs)
-        {
-            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
-        }
-
-        for (auto &m : masses)
-        {
-            if (!m->pinned)
-            {
-                Vector2D temp_position = m->position;
-                // TODO (Part 3.1): Set the new position of the rope mass
-                
-                // TODO (Part 4): Add global Verlet damping
-            }
-        }
-    }
+				// TODO (Part 4): Add global Verlet damping
+			}
+		}
+	}
 }
